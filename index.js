c9a572507578b938dc395751f0e53df1c340719f
Aaron Sanchez <saarong12@gmail.com>
2023-02-26 22:58:27 -0600

    created a roman numeral converter

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 // #1 Takes an array that has duplicates and only pushes the number ONCE
 
-const arr = [1, 3, 3, 4, 6, 7, 7]
+// const arr = [1, 3, 3, 4, 6, 7, 7]
 
-const loop = () => {
-  let noDupes = []
+// const loop = () => {
+//   let noDupes = []
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] != arr[i + 1]) {
-      noDupes.push(arr[i])
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] != arr[i + 1]) {
+//       noDupes.push(arr[i])
+//     }
+//   }
+
+//   console.log(noDupes)
+// }
+
+// loop()
+
+// #2 roman numeral converter
+
+let romanToInt = (roman) => {
+  let sym = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  }
+  let result = 0
+
+  for (let i = 0; i < roman.length; i++) {
+    const cur = sym[roman[i]]
+    const next = sym[roman[i + 1]]
+
+    if (cur < next) {
+      result += next - cur
+      i++
+    } else {
+      result += cur
     }
   }
 
-  console.log(noDupes)
+  console.log(result)
 }
 
-loop()
+romanToInt('V')
